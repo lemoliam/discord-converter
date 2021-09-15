fb45b70e378bde1635b2db450a1324bc84195734
lemoliam <yajafromhell@gmail.com>
2021-09-15 18:14:53 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+login_data = input('Enter login data: test@gmail.com password123\n')
+array = login_data.split(' ')
+mail = array[0]
+password = array[1]
+
+
+def login(mail, password):
+    payload = {
+        'email': mail,
+        'password': password,
+        'undelete': 'false',
+        'captcha_key': 'null'
+    }
+    r = requests.post('https://discordapp.com/api/v9/auth/login', json=payload)
+    j = json.loads(r.content)
+    print(j['token'] if 'token' in j else 'No token found')
+
+
+login(mail, password)
